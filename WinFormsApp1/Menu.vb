5bc1d9562fd37c033fd0030cbb8221a1742e13b8
suratinkru <toey0908toey@gmail.com>
2022-11-01 17:41:40 +0700

    gg

--- WinFormsApp1/Menu.vb
+++ WinFormsApp1/Menu.vb
@@ -15,4 +15,12 @@
         Login.ShowDialog()
 
     End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub Button4_Click_1(sender As Object, e As EventArgs) Handles Button4.Click
+
+    End Sub
 End Class
