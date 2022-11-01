5c76edd3970688f900366b3672c4030b7ba4a649
anongnat <anongnat6952@gmail.com>
2022-11-01 17:26:03 +0700

    up

---
+++ WinFormsApp1/Page_for_employees2.vb
@@ -0,0 +1,5 @@
+﻿Public Class Page_for_employees2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Page_for_employees.ShowDialog()
+    End Sub
+End Class
